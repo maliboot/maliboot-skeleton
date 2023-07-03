4fa3188cbf0a8806d3e1ad25674a4021d7515fca
stone <shilei_zhang@163.com>
2023-07-03 10:12:17 +0800

    other(master): 优化日志配置

--- config/autoload/logger.php
+++ config/autoload/logger.php
@@ -1,30 +1,76 @@
 <?php
 
 declare(strict_types=1);
-/**
- * This file is part of Hyperf.
- *
- * @link     https://www.hyperf.io
- * @document https://hyperf.wiki
- * @contact  group@hyperf.io
- * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
- */
-return [
+
+use Monolog\Formatter\LogstashFormatter;
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Level;
+use Hyperf\Support;
+
+$appName = Support\env('APP_NAME', 'maliboot');
+$defaultLogDir = '';
+if (in_array(Support\env('APP_ENV', 'production'), ['production', 'gray'])) {
+    $defaultLogDir = '/var/www-log';
+} else {
+    $defaultLogDir = './runtime';
+}
+$logDir = rtrim(Support\env('WWW_LOG_DIR', $defaultLogDir), '/') . '/' . $appName . '/logs/';
+$formatter = [
+    'class' => LogstashFormatter::class,
+    'constructor' => [
+        'applicationName' => $appName,
+    ],
+];
+
+$handlers = [
     'default' => [
-        'handler' => [
-            'class' => Monolog\Handler\StreamHandler::class,
-            'constructor' => [
-                'stream' => BASE_PATH . '/runtime/logs/hyperf.log',
-                'level' => Monolog\Logger::DEBUG,
+        'handlers' => [
+            [
+                'class' => RotatingFileHandler::class,
+                'constructor' => [
+                    'filename' => $logDir . '/info.log',
+                    'level' => Level::Info,
+                ],
+                'formatter' => $formatter,
             ],
-        ],
-        'formatter' => [
-            'class' => Monolog\Formatter\LineFormatter::class,
-            'constructor' => [
-                'format' => null,
-                'dateFormat' => 'Y-m-d H:i:s',
-                'allowInlineLineBreaks' => true,
+            [
+                'class' => RotatingFileHandler::class,
+                'constructor' => [
+                    'filename' => $logDir . '/error.log',
+                    'level' => Level::Error,
+                ],
+                'formatter' => $formatter,
+            ],
+            [
+                'class' => RotatingFileHandler::class,
+                'constructor' => [
+                    'filename' => $logDir . '/warning.log',
+                    'level' => Level::Warning,
+                ],
+                'formatter' => $formatter,
+            ],
+            [
+                'class' => RotatingFileHandler::class,
+                'constructor' => [
+                    'filename' => $logDir . '/critical.log',
+                    'level' => Level::Critical,
+                ],
+                'formatter' => $formatter,
             ],
         ],
     ],
 ];
+
+$appEnv = Support\env('APP_ENV', 'production');
+if (in_array($appEnv, ['test', 'local', 'dev'])) {
+    $handlers['default']['handlers'][] = [
+        'class' => RotatingFileHandler::class,
+        'constructor' => [
+            'filename' => $logDir . '/debug.log',
+            'level' => Level::Debug,
+        ],
+        'formatter' => $formatter,
+    ];
+}
+
+return $handlers;
